534f6a8982e7c2a6270e5d5bc6f43872ed41008f
mytvdor <mytvdor@gmail.com>
2026-05-14 21:16:36 +0530

    seo

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -5,6 +5,27 @@ export default function sitemap() {
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
+      },
+      
+     
+
+    {
+      url: "https://www.thevision9.com/about",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.thevision9.com/services",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.thevision9.com/contact",
+      lastModified: new Date(),
+      },
+    {
+      url: "https://www.thevision9.com/client",
+      lastModified: new Date(),
     },
   ];
 }
